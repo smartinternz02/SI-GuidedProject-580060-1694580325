2333e22563136b9536a50567d4ce338dccece9de
viswaskomati <144535784+viswaskomati@users.noreply.github.com>
2023-09-13 23:47:40 +0530

    Update MainActivity.kt

--- assignment3/loginapp/app/src/main/java/com/example/loginapp/MainActivity.kt
+++ assignment3/loginapp/app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -42,10 +42,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LoginAppTheme() {
-                // A surface container using the 'background' color from the theme
+               
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = backgroundColor // Use backgroundColor here
+                    color = backgroundColor 
                 ) {
                     LoginScreen()
                 }
